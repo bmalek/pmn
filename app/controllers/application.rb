1e257f115626ee475cf3222b81e6560eb4bce3f2
Behzad <behzad.malek@gmail.com>
2011-01-20 21:35:52 -0500

    created a RESTful user model that handles the cellphone verification process before storing data into the database. Then, removed the verify controller and cleaned up a little

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,4 +12,17 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   filter_parameter_logging :password
+
+ private
+
+ def logged_in?
+   unless session[:user]
+     flash[:notice] = "Please login first!"
+     redirect_to :controller => 'account', :action => 'login'
+   else
+    return true
+   end
+ end # end of logged_in?
+
+
 end
