9c7fa468b644549ad75cff44a410873bb0064554
Behzad <behzad.malek@gmail.com>
2011-02-18 02:16:45 -0500

    fixed the menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.xml
   def show
-    @user = User.find_by_id(params[:id]) 
+    #@user = User.find_by_id(params[:id]) 
 
     respond_to do |format|
       format.html # show.html.erb
